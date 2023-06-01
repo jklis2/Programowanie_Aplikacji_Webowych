f6d86cf160933178647874857ab36fcceae5ad14
Jakub Kliś <jakubklis2201@gmail.com>
2023-06-01 16:27:34 +0200

    Functionalities delete

--- ManageMe/src/app/dashboard/dashboard-content/dashboard-content.component.ts
+++ ManageMe/src/app/dashboard/dashboard-content/dashboard-content.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { projectDetails } from './project-details';
 
 @Component({
   selector: 'app-dashboard-content',
   templateUrl: './dashboard-content.component.html',
-  styleUrls: ['./dashboard-content.component.scss']
+  styleUrls: ['./dashboard-content.component.scss'],
 })
 export class DashboardContentComponent {
+  projectList = projectDetails;
 
+  deleteFunctionality(id: any) {
+    // Find the index of the functionality with the given ID
+    const functionalityIndex = this.projectList[0].functionallities.findIndex(
+      (func) => func.functionallityID === id
+    );
+
+    if (functionalityIndex !== -1) {
+      this.projectList[0].functionallities.splice(functionalityIndex, 1);
+    }
+  }
 }
---
+++ ManageMe/src/app/dashboard/dashboard-content/project-details.ts
@@ -0,0 +1,53 @@
+export const projectDetails = [
+    {
+        id: 1,
+        title: 'Logic Games 4Fun',
+        description: 'We are a startup that wants to show people that browser games do not have to be violent, addictive or pointless. Logic Games 4Fun is created to encourage people to practice their mind. Our logic games will be adapted to people of all ages, through different levels of difficulty.',
+        functionallities: [
+            {
+                functionallityID: 1,
+                functionallityName: 'Create Sudoku Game',
+                tasks: [
+                    {
+                        taskID: 1,
+                        taskName: 'Logic for Sudoku',
+                        status: 'ToDo'
+                    }
+                ]
+            },
+            {
+                functionallityID: 2,
+                functionallityName: 'Create Memory Game',
+                tasks: [
+                    {
+                        taskID: 1,
+                        taskName: 'Logic for Memory',
+                        status: ' 2 ToDo'
+                    }
+                ]
+            },
+            {
+                functionallityID: 3,
+                functionallityName: 'Create Crossword Game',
+                tasks: [
+                    {
+                        taskID: 1,
+                        taskName: 'Logic for Crossword',
+                        status: ' 2 ToDo'
+                    }
+                ]
+            },
+            {
+                functionallityID: 4,
+                functionallityName: 'Create Puzzle Game',
+                tasks: [
+                    {
+                        taskID: 1,
+                        taskName: 'Logic for Puzzle',
+                        status: ' 2 ToDo'
+                    }
+                ]
+            }
+        ]
+    }
+];
